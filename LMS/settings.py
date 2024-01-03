ce3db473ead13b1156ae2c6ae7793135822e14d6
shehjad0 <bettercallshehjad@gmail.com>
2024-01-04 00:22:04 +0600

    ...

--- LMS/settings.py
+++ LMS/settings.py
@@ -28,6 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+CSRF_TRUSTED_ORIGINS=['https://simpleonlinelibrary.onrender.com', 'http://127.0.0.1']
 
 ALLOWED_HOSTS = ["*"]
 
